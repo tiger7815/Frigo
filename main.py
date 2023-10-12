2c8dafe8708e043eb66f95877d9f82f11aa06b64
Lord <110597375+kd2929@users.noreply.github.com>
2023-10-12 11:13:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
+from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
 import requests
 import json
 import subprocess
-from pyrogram import Client
+from pyrogram import Client, filters
 from pyrogram.types.messages_and_media import message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import FloodWait
 from pyromod import listen
 from pyrogram.types import Message
-import pyrogram
 from pyrogram import Client, filters
-import tgcrypto
 from p_bar import progress_bar
 from subprocess import getstatusoutput
+from aiohttp import ClientSession
 import helper
-import logging
+from logger import logging
 import time
-import aiohttp
 import asyncio
-import aiofiles
 from pyrogram.types import User, Message
+from config import *
 import sys
 import re
 import os
